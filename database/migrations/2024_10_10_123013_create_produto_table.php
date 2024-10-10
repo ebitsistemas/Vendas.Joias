b9c7253406f5be5132f3d7121626a218b965eff0
Jeferson Maciel <ti@ebitsistemas.com.br>
2024-10-10 10:09:18 -0300

    Migration

---
+++ database/migrations/2024_10_10_123013_create_produto_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255)->nullable();
+            $table->string('slug', 255)->nullable();
+            $table->text('descricao')->nullable();
+            $table->string('descricao_curta', 255)->nullable();
+            $table->integer('unidade_id')->nullable();
+            $table->integer('categoria_id')->nullable();
+            $table->string('codigo_interno', 60)->nullable();
+            $table->string('codigo_barras', 60)->nullable();
+            $table->double('preco_custo', 10, 2)->nullable();
+            $table->double('custos_adicionais', 10, 2)->nullable();
+            $table->double('margem_lucro', 10, 2)->nullable();
+            $table->double('comissao_venda', 10, 2)->nullable();
+            $table->double('descontos', 10, 2)->nullable();
+            $table->double('preco_venda_sugerido', 10, 2)->nullable();
+            $table->double('preco_venda', 10, 2)->nullable();
+            $table->text('imagem')->nullable();
+            $table->boolean('status')->default(1)->comment('2 - Bloqueado; 1 - Ativo; 0 - Inativo;');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
---
+++ database/migrations/2024_10_10_123132_create_venda_item_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendas_itens', function (Blueprint $table) {
+            $table->id();
+            $table->integer('venda_id')->nullable();
+            $table->integer('produto_id')->nullable();
+            $table->double('valor_unitario', 10, 2)->nullable();
+            $table->double('quantidade', 10, 2)->nullable();
+            $table->integer('tipo_desconto')->nullable();
+            $table->double('valor_desconto_real', 10, 2)->nullable();
+            $table->double('valor_desconto_percentual', 10, 2)->nullable();
+            $table->double('valor_total', 10, 2)->nullable();
+            $table->boolean('status')->default(1)->comment('2 - Bloqueado; 1 - Ativo; 0 - Inativo;');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendas_itens');
+    }
+};

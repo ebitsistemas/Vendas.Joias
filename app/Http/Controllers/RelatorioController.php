4f2c08b310738e51e0a364273ad170e3d2311c23
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-01-31 20:55:28 -0300

    Commit Initial

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -95,23 +95,23 @@ class RelatorioController extends Controller
                            `clientes`.`dia_cobranca`,
                            (SELECT SUM(vendas.saldo) FROM vendas WHERE `vendas`.`cliente_id` = `clientes`.`id`) as saldo,  /* AND date(`vendas`.`data_venda`) >= '{$ano}-{$mes}-01' */
                            (SELECT vendas_cobrado.status FROM vendas_cobrado WHERE vendas_cobrado.cliente_id = clientes.id AND vendas_cobrado.data = '{$ano}-{$mes}-01') as cobrado_status
-                    FROM `clientes` WHERE `clientes`.`deleted_at` is null ";
+                    FROM `clientes` WHERE true ";
         if (!empty($request->tipo_pessoa)) {
-            $sql .= "and clientes.tipo_pessoa = {$request->tipo_pessoa}";
+            $sql .= "and clientes.tipo_pessoa = {$request->tipo_pessoa} ";
         }
         if (!empty($request->grupo_id)) {
-            $sql .= "and clientes.grupo_id = {$request->grupo_id}";
+            $sql .= "and clientes.grupo_id = {$request->grupo_id} ";
         }
         if (!empty($request->cidade)) {
-            $sql .= "and clientes.cidade = {$request->cidade}";
+            $sql .= "and clientes.cidade = {$request->cidade} ";
         }
         if (!empty($request->bairro)) {
-            $sql .= "and clientes.bairro = {$request->bairro}";
+            $sql .= "and clientes.bairro = {$request->bairro} ";
         }
         if (!empty($request->status)) {
-            $sql .= "and clientes.status = {$request->status}";
+            $sql .= "and clientes.status = {$request->status} ";
         }
-        //$sql .= "and `clientes`.`deleted_at` is null ";
+        $sql .= "and `clientes`.`deleted_at` is null ";
         $sql .= "group by `clientes`.`id`) as dados WHERE saldo > 0";
 
         $clientes = \DB::select($sql);

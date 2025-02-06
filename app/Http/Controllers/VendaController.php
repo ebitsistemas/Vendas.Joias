9de5ce1e0780be1933aa8caa1aecb1448e54af80
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-02-06 19:22:20 -0300

    Commit Initial

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -221,8 +221,8 @@ class VendaController extends Controller
             ->sum('valor_subtotal');
 
         $venda = Venda::find($venda_id);
-        $venda->saldo = $venda->total_liquido - $faturas;
-        $venda->status = ($venda->total_liquido - $faturas) <= 0 ? 1 : 0;
+        $venda->saldo = $venda->total_liquido - floatval($faturas);
+        $venda->status = ($venda->total_liquido - floatval($faturas)) <= 0 ? 1 : 0;
         $venda->save();
     }
 

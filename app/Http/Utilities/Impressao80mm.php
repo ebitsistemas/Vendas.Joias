54f925ec420151fa8d56babd877785d699e09c06
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-04-11 09:30:39 -0300

    Commit Initial

--- app/Http/Utilities/Impressao80mm.php
+++ app/Http/Utilities/Impressao80mm.php
@@ -634,31 +634,32 @@ class Impressao80mm
             $pdf->Cell(18, 1, 'R$ ' . number_format($venda->total_liquido, 2, ',', '.'), 0, 0, 'R', true);
         }*/
 
+        $pagamentos = array_reverse($pagamentos->toArray());
         foreach ($pagamentos as $pagamento) {
-            if ($pagamento->tipo == 'venda') {
+            if ($pagamento['tipo'] == 'venda') {
                 $height += 5;
                 $pdf->setY($height);
                 $pdf->setX(2);
                 $pdf->SetFont('Arial', '', 8);
-                $pdf->Cell(35, 1, utf8_decode('VENDA #'.$pagamento->venda->id), 0, 0, 'L', true);
-                $pdf->Cell(24, 1, date('d/m/Y', strtotime($pagamento->venda->data_venda)), 0, 0, 'L', true);
-                $pdf->Cell(18, 1, 'R$ ' . number_format($pagamento->venda->total_liquido, 2, ',', '.'), 0, 0, 'R', true);
-            } else if ($pagamento->tipo == 'pagamento') {
+                $pdf->Cell(35, 1, utf8_decode('VENDA #'.$pagamento['venda']['id']), 0, 0, 'L', true);
+                $pdf->Cell(24, 1, date('d/m/Y', strtotime($pagamento['venda']['data_venda'])), 0, 0, 'L', true);
+                $pdf->Cell(18, 1, 'R$ ' . number_format($pagamento['venda']['total_liquido'], 2, ',', '.'), 0, 0, 'R', true);
+            } else if ($pagamento['tipo'] == 'pagamento') {
                 $height += 5;
                 $pdf->setY($height);
                 $pdf->setX(2);
                 $pdf->SetFont('Arial', '', 8);
                 $pdf->Cell(35, 1, utf8_decode('PAGAMENTO'), 0, 0, 'L', true);
-                $pdf->Cell(24, 1, date('d/m/Y', strtotime($pagamento->data_pagamento)), 0, 0, 'L', true);
-                $pdf->Cell(18, 1, ' - R$ ' . number_format($pagamento->valor_recebido, 2, ',', '.'), 0, 0, 'R', true);
-            } else if ($pagamento->tipo == 'saldo') {
+                $pdf->Cell(24, 1, date('d/m/Y', strtotime($pagamento['data_pagamento'])), 0, 0, 'L', true);
+                $pdf->Cell(18, 1, ' - R$ ' . number_format($pagamento['valor_recebido'], 2, ',', '.'), 0, 0, 'R', true);
+            } else if ($pagamento['tipo'] == 'saldo') {
                 $height += 5;
                 $pdf->setY($height);
                 $pdf->setX(2);
                 $pdf->SetFont('Arial', '', 8);
                 $pdf->Cell(35, 1, utf8_decode('SALDO ANTERIOR'), 0, 0, 'L', true);
                 $pdf->Cell(24, 1, ' ', 0, 0, 'L', true);
-                $pdf->Cell(18, 1, ' R$ ' . number_format($pagamento->valor_recebido, 2, ',', '.'), 0, 0, 'R', true);
+                $pdf->Cell(18, 1, ' R$ ' . number_format($pagamento['valor_recebido'], 2, ',', '.'), 0, 0, 'R', true);
             }
         }
 

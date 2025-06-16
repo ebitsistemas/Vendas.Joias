1f4fa0c94bdd8c18ce5e62164499b0f47c5beda5
macyel <ti@ebitsistemas.com.br>
2025-06-16 10:35:41 -0300

    Migração de dados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,8 +21,8 @@
                 <div class="card">
                     <a href="{{ url('') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-home text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Home</h4>
+                            <i class="fal fa-home text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Home</h4>
                         </div>
                     </a>
                 </div>
@@ -32,8 +32,8 @@
                 <div class="card">
                     <a href="{{ url('dashboard') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-chart-line text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Dashboard</h4>
+                            <i class="fal fa-chart-line text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Dashboard</h4>
                         </div>
                     </a>
                 </div>
@@ -43,8 +43,8 @@
                 <div class="card">
                     <a href="{{ url('cliente') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-user text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Clientes</h4>
+                            <i class="fal fa-user text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Clientes</h4>
                         </div>
                     </a>
                 </div>
@@ -54,8 +54,8 @@
                 <div class="card">
                     <a href="{{ url('produto') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-box text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Produtos</h4>
+                            <i class="fal fa-box text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Produtos</h4>
                         </div>
                     </a>
                 </div>
@@ -65,8 +65,8 @@
                 <div class="card">
                     <a href="{{ url('categoria') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-sitemap text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Categorias</h4>
+                            <i class="fal fa-sitemap text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Categorias</h4>
                         </div>
                     </a>
                 </div>
@@ -76,8 +76,8 @@
                 <div class="card">
                     <a href="{{ url('grupo') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-folder-tree text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Grupos</h4>
+                            <i class="fal fa-folder-tree text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Grupos</h4>
                         </div>
                     </a>
                 </div>
@@ -87,8 +87,8 @@
                 <div class="card">
                     <a href="{{ url('venda') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-shopping-cart text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Vendas</h4>
+                            <i class="fal fa-shopping-cart text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Vendas</h4>
                         </div>
                     </a>
                 </div>
@@ -98,8 +98,8 @@
                 <div class="card">
                     <a href="{{ url('usuario') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-user-shield text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Usuários</h4>
+                            <i class="fal fa-user-shield text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Usuários</h4>
                         </div>
                     </a>
                 </div>
@@ -109,8 +109,8 @@
                 <div class="card">
                     <a href="{{ url('configuracao') }}" class="card-body bg-theme-light-hover border border-2 p-2">
                         <div class="text-center py-3">
-                            <i class="fal fa-cogs text-theme fs-36px mb-2"></i>
-                            <h4 class="mb-1 text-theme fs-18px mt-3">Config.</h4>
+                            <i class="fal fa-cogs text-theme fs-44px mb-2"></i>
+                            <h4 class="mb-1 text-theme fs-26px mt-3">Config.</h4>
                         </div>
                     </a>
                 </div>

2cec73becb66b47fe7c4454d1a8c8eaabbfcdccd
macyel <ti@ebitsistemas.com.br>
2025-07-07 09:56:16 -0300

    Migração de dados

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -231,7 +231,7 @@ class ClienteController extends Controller
 
         $ultimasMovimentacoes = $todasAsMovimentacoes
             ->sortByDesc($funcaoOrdenadora)
-            ->take(40);
+            ->take(20);
         $movimentacoesOrdenadas = $ultimasMovimentacoes->sortBy($funcaoOrdenadora);
 
         $cliente = Cliente::find($request->id);
